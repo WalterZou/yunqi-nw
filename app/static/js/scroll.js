826694d05fef369f0967a14c45a6722b67e64ab7
WalterZou <olayi20u@gmail.com>
2013-09-18 17:25:56 +0800

    scroll

--- app/static/js/scroll.js
+++ app/static/js/scroll.js
@@ -1,16 +1,49 @@
 /**
-*  Author   : Walter Zou
-*  Created  : 2013-09-17 16:02
-*  Function : Custiomize scrollbar
-*/
+ *  Author   : Walter Zou
+ *  Created  : 2013-09-17 16:02
+ *  Function : Custiomize scrollbar
+ */
 'use strict';
-(function($){
-    $.fn.iScroll = function(opt){
-        return this.each(function(){
-            var scrollDiv=document.createElement('div');
-            scrollDiv.style.cssText='width:5px;height:10px;background:rgba(0;0;0;.5);border-radius:2px;position:absolute;top:42px;right:8px';
-            $(this).append(scrollDiv);
-            console.log($(this),this);
+(function ($) {
+    $.fn.iScroll = function (opt) {
+        return this.each(function () {
+            var scrollDiv = document.createElement('div');
+            scrollDiv.style.cssText = 'width:6px;height:30px;background:rgba(0,0,0,.3);border-radius:2px;position:absolute;top:43px;right:2px;z-index:9;display:none;';
+            $(this).after(scrollDiv);
+
+            function setScrollbar(elm) {
+                var h = elm.children().height(),
+                    _h = 0.1 * h;
+                _h > 20 ? $(scrollDiv).css('height', _h) : $(scrollDiv).css('height', '30px');
+                console.log(_h, h);
+            }
+            $(this).bind({
+                scroll: function () {
+                    var H = $(this).children().height(), //滚动的文档高度
+                        h = $(this).height(), //文档容器的高度
+                        scollTop = $(this).scrollTop(),
+                        top,
+                        _h;
+                    setScrollbar($(this));
+                    _h = $(scrollDiv).height();
+                    top = 42 + scollTop * (h - _h) / (H - h);
+                    if (scollTop == (H - h)) {
+                        top -= 1;
+                    } else if (scollTop == 0) {
+                        top = 43;
+                    }
+                    $(scrollDiv).css({
+                        top: top
+                    });
+                }
+            });
+            $(this).hover(function () {
+                setScrollbar($(this));
+                $(scrollDiv).fadeIn(500);
+            }, function () {
+                setScrollbar($(this));
+                $(scrollDiv).fadeOut(500);
+            });
         });
     };
-})(jQuery)
+})(jQuery);
